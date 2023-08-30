d295ec971c2e627540b0b34a3bb13fcf5c654bf5
alep-2000 <alessiopiras056@gmail.com>
2023-08-30 16:56:42 +0200

    Modify page dashboard and admin

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -11,7 +11,7 @@ use App\Models\Post;
 class PostController extends Controller
 {
     public function index(){
-        $posts = Post::with('types', 'tecnologies')->paginate(6);
+        $posts = Post::with('types', 'tecnologies')->paginate(3);
         return response()->json([
             'success'  => true,
             'results'  => $posts
